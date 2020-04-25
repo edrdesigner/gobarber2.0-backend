3546781ba3b3b79229a28ac8d6c79dd29abfe16e
eduardo.reichert <edrdesigner@gmail.com>
2020-04-25 15:08:14 -0300

    2 - Cadastro de usuarios

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import appointmentsRounter from './appointments.routes';
+import usersRounter from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRounter);
+routes.use('/users', usersRounter);
 
 routes.post('/users', (request, response) => {
   const { email, name } = request.body;
