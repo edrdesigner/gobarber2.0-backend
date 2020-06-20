c57f3eb1b187e6c1e160cf893142ffcf22218ed3
eduardo.reichert <edrdesigner@gmail.com>
2020-06-20 15:00:25 -0300

    feat: Emails em desenvolvinto utilizando Etherial

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,11 +2,17 @@ import { container } from 'tsyringe';
 
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-// import IMailProvider from './MailProvider/models/IMailProvider';
+
+import IMailProvider from './MailProvider/models/IMailProvider';
+import EtherialMailProvider from './MailProvider/implementations/EtherialMailProvider';
 
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
 );
 
-// container.registerSingleton<IMailProvider>('MailProvider', MailProvider);
+// Cria uma instancia pois tem um construtor sendo singleton
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherialMailProvider(),
+);
