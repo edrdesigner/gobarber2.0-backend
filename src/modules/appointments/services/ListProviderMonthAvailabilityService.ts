dfacc7a24420f704a5850a8189b09ee787e2d416
eduardo.reichert <edrdesigner@gmail.com>
2020-07-04 19:49:06 -0300

    Listando horarios disponiveis

---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,61 @@
+import { injectable, inject } from 'tsyringe';
+import { getDaysInMonth, getDate } from 'date-fns';
+
+// import User from '@modules/users/infra/typeorm/entities/User';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+/**
+ * [{day: 1, available: true}]
+ */
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProviderMonthAvailabilityService {
+  constructor(
+    @inject('IAppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    month,
+    year,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      { provider_id, month, year },
+    );
+
+    const numberOfDaysInMonths = getDaysInMonth(new Date(year, month - 1));
+
+    const eachDayArray = Array.from(
+      { length: numberOfDaysInMonths },
+      (_, index) => index + 1,
+    );
+
+    const availability = eachDayArray.map((day) => {
+      const appointmentsInDay = appointments.filter(
+        (appointment) => getDate(appointment.date) === day,
+      );
+
+      return {
+        day,
+        available: appointmentsInDay.length < 10,
+      };
+    });
+
+    return availability;
+  }
+}
+
+export default ListProviderMonthAvailabilityService;
